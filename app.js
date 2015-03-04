cd57dd9a3f603a436c1002b5524f7af9ff15ee8f
axplay0829@gmail.com <axplay0829@gmail.com>
2015-03-04 09:16:58 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,49 @@
+// we want to detect element value changes in an array, $watchColelction does not work for this case,
+// so we have to resort to $watch with objectEquality == true. This will cause performance
+
+angular.module('App', [])
+    .constant('LEN', 10000)
+    .factory('items', function(LEN) {
+        var items = [];
+        for (var i = 0; i < LEN; i++) {
+            items.push({
+                value: i
+            });
+        }
+        return items;
+    })
+    .controller('MainCtrl', function($scope, items, LEN) {
+        var vm = this;
+        vm.items = angular.copy(items);
+        $scope.$watch(function() {
+            return vm.items;
+        }, function(newVal, oldVal) {
+            if (newVal) {
+                console.log(vm.items[LEN / 2].value);
+                vm.mid = vm.items[LEN / 2].value;
+            }
+        }, true);
+
+        vm.addItem = function() {
+            vm.items[LEN / 2].value = vm.newItem;
+        }
+    })
+    .controller('SecondCtrl', function($scope, items, LEN) {
+        var vm = this;
+        vm.items = Immutable.List(angular.copy(items));
+
+        $scope.$watch(function() {
+            return vm.items;
+        }, function(newVal, oldVal) {
+            if (newVal) {
+                console.log(vm.items.get(LEN / 2).value);
+                vm.mid = vm.items.get(LEN / 2).value;
+            }
+        }, false);
+
+        vm.addItem = function() {
+            vm.items = vm.items.splice(LEN / 2, 1, {
+                value: vm.newItem
+            });
+        }
+    });
